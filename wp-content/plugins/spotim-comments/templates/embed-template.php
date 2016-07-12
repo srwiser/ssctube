1ca502f932f70a1b59105b0e0ecaec0ec4592281
srwiser <srwiser@gmail.com>
2016-07-12 17:33:29 -0400

    deleted unused plugins

--- wp-content/plugins/spotim-comments/templates/embed-template.php
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-    // v3.0.0
-    window.SPOTIM = {
-        spotId: <?php echo wp_json_encode( $this->get( 'spot_id' ) ); ?>,
-        parentElement: document.body,
-        options: {}
-    };
-</script>
-<script type="text/javascript" async src="//www.spot.im/launcher/bundle.js"></script>
